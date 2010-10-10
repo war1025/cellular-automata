6938206e566429ce4f2b7039d63e2af1d883bf8c
Wayne Rowcliffe <wrowclif@iastate.edu>
2010-10-09 23:55:03 -0500

    Working on a parser for 430 HW1

--- Fall 2010/430/HW1/cautamata/CAParser.cs
+++ Fall 2010/430/HW1/cautamata/CAParser.cs
@@ -6,6 +6,7 @@ namespace CAClient {
 
 		public string code;
 		public Color[] colors;
+		public uint defaultState;
 
 	}
 
@@ -16,7 +17,28 @@ namespace CAClient {
 		}
 
 		public CAComponents parseCASettings(string filename) {
+			var settings = File.OpenText(filename);
+			uint numStates = 0;
+			uint defaultState = 0;
+			string neighborhood = null;
+			string delta = null;
+			string colors = null;
 
+			string curLine = null;
+
+			while((curLine = settings.ReadLine()) != null) {
+				if(curLine.StartsWith("NumStates")) {
+					numStates = UInt32.Parse(curLine.Split(new char[] {':'}, 2)[1]);
+				} else if(curLine.StartsWith("DefaultState")) {
+					defaultState = UInt32.Parse(curLine.Split(new char[] {':'},2)[1];
+				} else if(curLine.StartsWith("Neighborhood")) {
+					neighborhood = curLine.Split(new char[] {':'},2)[1];
+				} else if(curLine.StartsWith("Colors")) {
+					colors = parseColors(curLine, settings);
+				} else if(curLine.StartsWith("Delta")) {
+					delta = parseDelta(curLine, delta);
+				}
+			}
 		}
 
 	}
