6f061d73496a82f541e730a18d987cebb401f029
Wayne Rowcliffe <wrowclif@iastate.edu>
2010-10-18 12:54:40 -0500

    Working on the code compiler for 430 HW1

--- Fall 2010/430/HW1/cautamata/CACompiler.cs
+++ Fall 2010/430/HW1/cautamata/CACompiler.cs
@@ -1,12 +1,28 @@
 
 using CAutamata;
 
+using System;
+using System.Collections.Generic;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Reflection;
+
 namespace CAServer {
 
 	public class CACompiler {
 
-		public static ICASettings compile(string code) {
-			return null;
+		public static ICASettings compile(string name, string code) {
+			var csCompiler = new CSharpCodeProvider();
+			var s = new string[] {code};
+			var compilerParams = new CompilerParameters();
+			compilerParams.GenerateExecutable = false;
+			compilerParams.GenerateInMemory = true;
+			var results = csCompiler.compileAssemblyFromSource(compilerParams, s);
+			if(results.Errors.HasErrors) {
+				return null;
+			}
+			var assembly = results.CompiledAssembly;
+			return assembly.CreateInstance(name) as ICASettings;
 		}
 
 	}
