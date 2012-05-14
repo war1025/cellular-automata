8971168a2e12c17af7305a053129e7a7084094c2
Wayne Rowcliffe <war1025@gmail.com>
2012-05-13 21:46:20 -0500

    Added some 430 HW1 stuff. Working on file parsers

---
+++ Fall 2010/430/HW1/cautamata/CAParser.cs
@@ -0,0 +1,24 @@
+
+
+namespace CAClient {
+
+	public struct CAComponents {
+
+		public string code;
+		public Color[] colors;
+
+	}
+
+	public class CAParser {
+
+		public CAParser() {
+
+		}
+
+		public CAComponents parseCASettings(string filename) {
+
+		}
+
+	}
+
+}
